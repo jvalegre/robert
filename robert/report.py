74f743c7e8dc9623fa82a6f10fe3b088efb1541a
David D <ddalmau@unizar.es>
2023-07-11 09:10:24 +0200

    1.Hot Fix

--- robert/report.py
+++ robert/report.py
@@ -18,6 +18,7 @@ General
 #    used for generating the final PDF report       #
 #####################################################.
 import os
+import textwrap
 from pathlib import Path
 import sys
 from robert.utils import load_variables
@@ -97,12 +98,12 @@ class report:
                     </span></p>
                     {module_data}
                     {html_png}
+                    
                     """
 
                 if i == len(modules) - 1:
                     data_lines += f"""
                         <p>
-                        <hr class="black">
                     """
 
         return data_lines
@@ -120,19 +121,15 @@ class report:
                     break
 
         header_lines = f"""
-            <h1 style="text-align: left; margin-bottom: 0.5em;">
-                <img src="file:///{self.args.path_icons}/Robert_logo.jpg" alt="" style="float:right; width:25%; margin-top: -5px;" />
-                <span style="vertical-align: middle;">Report</span>
-                <br>
-                <hr class="black">
+            <h1 style="text-align: center; margin-bottom: 0.5em;">
+                <img src="file:///{self.args.path_icons}/Robert_logo.jpg" alt="" style="display: block; margin-left: auto; margin-right: auto; width: 50%; margin-top: -12px;" />
                 <span style="font-weight:bold;"></span>
+            <p>
             </h1>
             <p style="text-align: center;"></p>
             <p style="text-align: justify;">
             <div class="dat-content"><pre>{header_dat}</pre></div>
             """
-
-
         return header_lines
 
 
@@ -156,82 +153,155 @@ def make_pdf(html, HTML, css_files):
 
 
 def css_content():
-    css_content = """
-    body {
+    # Obtain ROBERT version and CSV name
+    dat_file = Path(f'{os.getcwd()}/GENERATE/GENERATE_data.dat')
+    version = None
+    csv_name = None
+
+    if os.path.exists(dat_file):
+        with open(dat_file, 'r', errors="replace") as datfile:
+            for line in datfile:
+                if '--csv_name' in line:
+                    csv_name = line.split('--csv_name')[1].strip().split()[0]
+                    if csv_name.endswith('.csv'):
+                        break
+                    else:
+                        csv_name = None
+                elif version is None:
+                    version = line[:14].strip()
+
+    css_content = f"""
+    body {{
     font-size: 12px;
     line-height: 1.5;
-    }
-    @page {
+    }}
+    @page {{
         size: A4;
         margin: 2cm;
-        @bottom-right {
-            content: counter(page);
+        @bottom-right {{
+            content: "Page "counter(page) " of " counter(pages);
             font-size: 8pt;
             position: fixed;
             right: 0;
             bottom: 0;
-        }
-    }
-    * {
-        font-family: Arial, sans-serif;
-    }
-    .dat-content {
+            transform: translateY(-8pt);
+            white-space: nowrap;
+        }}  
+        @bottom-left {{
+            content: "{version}";
+            font-size: 8pt;
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            transform: translateY(-8pt);
+            white-space: nowrap;
+        }}
+        @bottom-center {{
+            content: "";
+            border-top: 3px solid black;
+            width: 100%;
+            position: fixed;
+            left: 0;
+            right: 0;
+            bottom: 0pt;
+            transform: translateY(8pt);
+        }}  
+        @top-center {{
+            content: "";
+            border-top: 3px solid black;
+            width: 100%;
+            position: fixed;
+            left: 0;
+            right: 0;
+            bottom: 0pt;
+            transform: translateY(40pt);
+        }}  
+        @top-left {{
+            content: "ROBERT Report";
+            font-size: 8pt;
+            font-weight:bold;
+            position: fixed;
+            position: fixed;
+            left: 0;
+            right: 0;
+            bottom: 0pt;
+            transform: translateY(2pt);
+            white-space: nowrap;
+        }} 
+        @top-right {{
+            content: "{csv_name}";
+            font-size: 8pt;
+            font-style: italic;
+            position: fixed;
+            position: fixed;
+            left: 0;
+            right: 0;
+            bottom: 0pt;
+            transform: translateY(2pt);
+            white-space: nowrap;
+        }} 
+    }}
+    * {{
+        font-family: "Helvetica", Arial, sans-serif;
+    }}
+    .dat-content {{
         width: 50%;
         max-width: 595pt;
         overflow-x: auto;
         line-height: 1.2;
-    }
+    }}
 
-    img[src="Robert_logo.jpg"] {
-        float: right;
-        width: 50%;
-    }
-    img[src*="Pearson"] {
+    img[src="Robert_logo.jpg"] {{
+        float: center;
+    }}
+    img[src*="Pearson"] {{
         display: inline-block;
         vertical-align: bottom;
         max-width: 48%;
         margin-left: 10px;
         margin-bottom: -5px;
-    }
-    img[src*="PFI"] {
+    }}
+    img[src*="PFI"] {{
         display: inline-block;
         vertical-align: bottom;
         max-width: 48%;
         margin-left: 10px;
         margin-bottom: -5px;
-    }
+    }}
 
-    img[src*="PFI"]:first-child {
+    img[src*="PFI"]:first-child {{
         margin-right: 10%;
-    }
-    .img-PREDICT {
+    }}
+    .img-PREDICT {{
         margin-top: 20px;
-    }
+    }}
     
-    hr.black {
+    hr.black {{
     border: none;
-    height: 1px;
+    height: 3px;
     background-color: black;
-    }
-    hr {
+    }}
+    
+    hr {{
     border: none;
     height: 1px;
     background-color: gray;
-    }
+    }}
 
-    body:before {
+    body:before {{
     top: 1.2cm;
-    }
+    }}
     """
     return css_content
 
 
-def insert_newlines(string, every=100):
+def insert_newlines(string, every=105):
     """Inserts a newline character every `every` characters in the string."""
-    return '\n'.join(string[i:i+every] for i in range(0, len(string), every))
+    wrapped_lines = textwrap.wrap(string, width=every)
+    return '\n'.join(wrapped_lines)
 
 
-def format_data(file_path, start_str, end_str, every=100):
+def format_data(file_path, start_str, end_str, every=105):
     """Reads a file and returns a formatted string between two markers."""
     with open(file_path, 'r', errors="replace") as file:
         content = file.read()
@@ -240,8 +310,15 @@ def format_data(file_path, start_str, end_str, every=100):
         end_pos = content.find("\n", end_pos)
         data = content[start_pos:end_pos]
         lines = data.split('\n')
-        formatted_lines = [
-            insert_newlines('    ' + line[4:] if line.startswith('- ') else line, every)
-            for line in lines
-        ]
+        formatted_lines = []
+        max_width = 110
+        for line in lines:
+            if line.startswith('- '):
+                line = '    ' + line[4:]
+            if line.startswith('-------') and line.endswith('-------'):
+                formatted_line = '<strong>' + line + '</strong>'
+            else:
+                formatted_line = textwrap.fill(line, width=max_width, subsequent_indent=' '*4)
+            formatted_lines.append(formatted_line)
         return '\n'.join(formatted_lines)
+
